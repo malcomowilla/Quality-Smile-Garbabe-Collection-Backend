8a653f7cffe29f6099a2221d3cf9c813d3196e4e
malcomowilla <owillamalcom@gmail.com>
2024-07-03 18:59:58 +0300

    deploying to render14

--- app/mailers/customer_code_mailer.rb
+++ app/mailers/customer_code_mailer.rb
@@ -5,7 +5,7 @@ class CustomerCodeMailer < ApplicationMailer
 
 def customer_code(customer)
     @customer = customer
-    MailtrapService.new(ENV['MAIL_TRAP_API_KEY']).send_template_email(
+    MailtrapService.new(ENV['MAIL_TRAP_API_KEY'] || 'f17620673c51e537ef268dea49025da8').send_template_email(
       to: @customer.email,
       template_uuid: ENV['MAIL_TRAP_TEMPLATE_UUID'],
       template_variables: {
