56f9f5b8d3ab922ab943aacccf0c6a255008b803
malcomowilla <owillamalcom@gmail.com>
2024-11-11 11:55:12 +0300

    aitechs garbage collection backend to prod7

--- db/migrate/20240727144353_remove_admin_id_from_prefix_and_digits_for_service_provider.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAdminIdFromPrefixAndDigitsForServiceProvider < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :prefix_and_digits_for_service_providers, :admin_id, :integer
-  end
-end
