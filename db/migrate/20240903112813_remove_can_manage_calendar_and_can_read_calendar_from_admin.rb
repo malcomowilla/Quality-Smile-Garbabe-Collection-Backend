3b799e7f70acd80d2453bdf0538b3f5eee9955c7
malcomowilla <owillamalcom@gmail.com>
2024-11-11 19:17:55 +0300

    deploying saas garbage collection backend2

--- db/migrate/20240903112813_remove_can_manage_calendar_and_can_read_calendar_from_admin.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCanManageCalendarAndCanReadCalendarFromAdmin < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :admins, :can_manage_calendar, :string
-    remove_column :admins, :can_read_calendar, :string
-  end
-end
