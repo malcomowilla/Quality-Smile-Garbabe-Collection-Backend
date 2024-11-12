98e49521e09181e600e2649891f208948c1a2e48
malcomowilla <owillamalcom@gmail.com>
2024-11-12 19:17:57 +0300

    testing multitenancy14

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -24,7 +24,7 @@ require 'message_template'
 
 
   def set_tenant
-    @account = Account.find_or_create_by(domain:request.domain, subdomain: request.subdomain)
+    @account = Account.find_or_create_by(subdomain: request.subdomain)
   
     set_current_tenant(@account)
   rescue ActiveRecord::RecordNotFound
