668d0d6b73878eaab0652107654af32cb52ebd73
malcomowilla <owillamalcom@gmail.com>
2024-11-12 18:39:01 +0300

    testing multitenancy11

--- app/controllers/company_settings_controller.rb
+++ app/controllers/company_settings_controller.rb
@@ -15,7 +15,7 @@ class CompanySettingsController < ApplicationController
   # GET /company_settings or /company_settings.json
   def index
     # @company_settings = CompanySetting.first
-    @account = Account.find_or_create_by(domain:request.domain, subdomain: request.subdomain)
+    @account = Account.find_or_create_by(subdomain: request.subdomain)
     @company_settings = @account.company_setting
     # render json: @company_settings
     render json: {
