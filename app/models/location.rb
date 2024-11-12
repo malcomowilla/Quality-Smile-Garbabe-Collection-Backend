1abac5d2a560cd8b496cbf2426b7c281ab248d58
malcomowilla <owillamalcom@gmail.com>
2024-11-12 19:36:03 +0300

    testing multitenancy16

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,7 +3,7 @@ class Location < ApplicationRecord
     validates :location_name, presence: true, uniqueness: true
 
     acts_as_tenant(:account)
-
+belongs_to :account
 
 
 end
--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,3 +1,5 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :location_name, :sub_location, :location_code, :category
+  attributes :id, :location_name, :sub_location, :location_code, :category, :account_id
+  belongs_to :account
+
 end
