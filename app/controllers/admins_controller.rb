7e78da4ef36052ac7c560f8d8ecd4c09010c733d
malcomowilla <owillamalcom@gmail.com>
2024-11-14 13:36:48 +0300

    webauthn5

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -442,7 +442,7 @@ def register_webauthn
         user: { id: Base64.urlsafe_encode64(@the_admin.webauthn_id), name: @the_admin.user_name || @the_admin.email },
         exclude: @the_admin.credentials.map { |c| c.webauthn_id },
         rp: { name: 'aitechs', id: request.headers['X-Original-Host'] },
-        origin: request.headers['X-Original-Host'] # Add the origin here
+        # origin: request.headers['X-Original-Host']
 
       )
 
